ee81c44572c9c8f7588181f9cdb3d67206bc4989
Rabbi Hosain <56104056+rabbihosain@users.noreply.github.com>
2020-10-04 12:38:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    nav: false,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+});
